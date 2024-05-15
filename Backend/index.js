bdf9212b903e4916bdb768439d3684d7b20be49f
Subrat Pandey <subratpandey974@gmail.com>
2024-05-15 17:03:04 -0500

    Connected with MongoDB database

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,35 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://aayush:aayush@firstone.qbq9qbu.mongodb.net/mydbbb?retryWrites=true&w=majority";
 
+// Define the MongoDB connection string
+const mongoURI = "mongodb+srv://aayush:aayush@firstone.qbq9qbu.mongodb.net/mydbbb?retryWrites=true&w=majority";
 const express = require('express');
 const cors = require('cors');
 const router = require('./Routes/router');
 
+// Create an instance of express to start a new Express application
 const app = express();
+
 const port = 3001;
 
+// Use CORS to allow cross-origin requests
 app.use(cors());
+
+// Use express.json() middleware to parse JSON requests
 app.use(express.json());
+
+// Add router middleware to manage API routes
 app.use(router);
 
-// Connect to MongoDB
+// Connect to MongoDB using the mongoose connect method with the URI and connection options
 mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true,     // Use the new URL parser to avoid deprecation warnings
+    useUnifiedTopology: true  
 })
-.then(() => {
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+.then(() => { // After successful connection to the database
+    app.listen(port, () => console.log(`Server running on port ${port}`)); // Start the server on the defined port
 })
-.catch(err => console.log(err));
+.catch(err => console.log(err)); // Log any errors that occur during the connection process
+
 
 
 
